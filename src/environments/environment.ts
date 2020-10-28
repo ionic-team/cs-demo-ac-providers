525e842744d8322399707c8980bd7923d9d71e28
Ken Sodemann <ken@ionic.io>
2020-10-28 07:57:44 -0500

    fix(cognito): use PKCE for web
    When using implicit flow, Cognito will produce a CORS error on refresh.
    This is circumvented by using PKCE on web.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -45,6 +45,7 @@ export const environment: Environment = {
     logoutUrl: '',
     iosWebView: 'private',
     logLevel: 'DEBUG',
+    webAuthFlow: 'PKCE',
   },
   okta: {
     authConfig: 'general',
